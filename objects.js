de18e6b3b845fd5873f6936eacd8d28f8431c416
Peder Goodman <peder.goodman@gmail.com>
2023-05-30 14:53:09 -0700

    followed along with video

--- objects.js
+++ objects.js
@@ -1,1 +1,28 @@
 console.log('Object Literals');
+
+
+let card0 = {
+    suit: 'Hearts',
+    number: 2
+} // end card0
+
+let card1 = {
+    suit: "Spades",
+    number: 1
+}
+
+
+function isRed( cardToCheck ) {
+    console.log('is red:', cardToCheck);
+    if (cardToCheck.suit === 'Hearts' || cardToCheck.suit === "Diamonds") {
+        return true;
+    } // end red 
+    else {
+        return false;
+    } // end !red
+} // end isRed
+
+console.log(isRed(card0));
+console.log(isRed(card1));
+
+
