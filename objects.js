5b6bbbc8d261d38c3a56e7abb2390ede30518d26
Peder Goodman <peder.goodman@gmail.com>
2023-05-30 15:06:27 -0700

    added function to compare 2 cards checking for which has a higher number

--- objects.js
+++ objects.js
@@ -9,7 +9,15 @@ let card0 = {
 let card1 = {
     suit: "Spades",
     number: 1
-}
+} // end card1
+
+let card2 = {
+    suit: 'Diamonds',
+    number: 4
+    
+} // end card2
+
+
 
 
 function isRed( cardToCheck ) {
@@ -22,7 +30,23 @@ function isRed( cardToCheck ) {
     } // end !red
 } // end isRed
 
+
+
+
 console.log(isRed(card0));
 console.log(isRed(card1));
 
 
+
+function higherCard (card_1, card_2) {
+    if (card_1.number < card_2.number) {
+        console.log(card_2);
+    }
+    else {
+        console.log(card_1);
+    }
+}
+
+higherCard (card0, card1)
+
+higherCard (card2, card1)
